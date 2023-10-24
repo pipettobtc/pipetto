f2d23164ed71cf9c9436320bb55cb4cf64950e84
pipettobtc <pipettobtc@proton.me>
2023-10-24 17:08:53 +0200

    improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from 'gatsby'
-import { Box, Grid, Image, InfiniteScroll, Button } from "grommet";
+import { Box, Grid, Image, InfiniteScroll, Button, Heading } from "grommet";
 
 const pageStyles = {
   color: "#232129",
@@ -10,7 +10,7 @@ const pageStyles = {
 
 const IndexPage = ({data}) => {
   const { allFile } = data
-  const fileNames = allFile.edges.map(({ node }) => node.relativePath)
+  const fileNames = allFile.edges.map(({ node }) => node.relativePath)?.sort((a, b) => a.localeCompare(b, undefined, { numeric: true }))
   console.log(fileNames)
   return (
     <main style={pageStyles}>
@@ -20,16 +20,19 @@ const IndexPage = ({data}) => {
         justify="center"
         className="pb-8"
       >
-        <Button primary label="Mint" href="https://pipe.inscrib3.land/open-art"/>
+        <Button primary label="Mint" href="https://pipe.inscrib3.land/create-unique-art"/>
       </Box>
       <Grid columns="small" gap="large">
         <InfiniteScroll items={fileNames}
           step={20}
         >
-                    {(el) => (
+          {(el,i) => (
           <Box border={{ color: "brand", size: "small" }} key={el}>
             <Link to={`https://pipettobtc.github.io/pipetto/${el}`}>
-              <Image src={`/pipetto/${el}`} fit="contain" style={{ imageRendering: "pixelated" }} />
+              <Box background="brand" align="center">
+                <Heading level={4}>ID: {i}</Heading>
+              </Box>
+              <Image src={`/${el}`} fit="contain" style={{ imageRendering: "pixelated" }} />
             </Link>
           </Box>
           )}
